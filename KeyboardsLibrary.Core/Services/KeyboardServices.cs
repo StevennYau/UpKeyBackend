5cbae5adaf7cd914c1f4ce9eec9b813e2534138a
StevennYau <stevenyau@gmail.com>
2021-04-27 15:53:11 -0400

    added methods to store scraped data

--- KeyboardsLibrary.Core/Services/KeyboardServices.cs
+++ KeyboardsLibrary.Core/Services/KeyboardServices.cs
@@ -23,14 +23,58 @@ namespace KeyboardsLibrary.Core
             return kb;
         }
 
-        public EbayKeyboard getKeyboard(string id)
+        public EbayKeyboard GetKeyboard(string id)
         {
             return _keyboards.Find(kb => kb.Id == id).First();
         }
 
-        public EbayKeyboard getKeyboardEbayId(string id)
+        public EbayKeyboard GetKeyboardEbayId(string id)
         {
             return _keyboards.Find(kb => kb.EbayId == id).First();
+        }
+
+        public void DeleteKeyboard(string id)
+        {
+            _keyboards.DeleteOne(kb => kb.Id == id);
+        }
+
+        public void DeleteKeyboardEbayId(string id)
+        {
+            _keyboards.DeleteOne(kb => kb.EbayId == id);
+        }
+
+        public EbayKeyboard UpdateKeyboard(EbayKeyboard kb)
+        {
+            GetKeyboardEbayId(kb.EbayId);
+            _keyboards.ReplaceOne(kbs => kbs.EbayId == kb.EbayId, kb);
+            return kb;
+        }
+
+        public List<EbayKeyboard> StoreAndUpdate(List<EbayKeyboard> KbList)
+        {
+            foreach (var kb in KbList)
+            {
+                EbayKeyboard compare = _keyboards.Find(keyb => keyb.EbayId == kb.EbayId).FirstOrDefault();
+
+                if (compare == null)
+                {
+                    Console.WriteLine("DOENST EXIST SO POST");
+                    Console.WriteLine(kb.Name);
+                    _keyboards.InsertOne(kb);
+                }
+                else // update since compare was found
+                {
+                    Console.WriteLine("UPDATE");
+                    Console.WriteLine(compare.Name);
+                    Console.WriteLine(kb.Name);
+                    Console.WriteLine(compare.EbayId);
+                    Console.WriteLine(kb.EbayId);
+                    kb.Id = compare.Id;
+                    _keyboards.ReplaceOne(kbs => kbs.EbayId == kb.EbayId, kb);
+                }
+            }
+
+            return KbList;
         }
     }
 }
