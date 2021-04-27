73878499751da325d253138684b314b36c82e92f
StevennYau <stevenyau@gmail.com>
2021-04-26 20:04:41 -0400

    config database settings

---
+++ KeyboardsLibrary.Core/KeyboardsDbConfig.cs
@@ -0,0 +1,9 @@
+namespace KeyboardsLibrary.Core
+{
+    public class KeyboardsDbConfig
+    {
+        public string Database_Name { get; set; }
+        public string Keyboard_Collection_Name { get; set; }
+        public string Connection_String { get; set; }
+    }
+}
