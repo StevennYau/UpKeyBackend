e3783b36d2fec176877d4a862092378e09998448
StevennYau <stevenyau@gmail.com>
2021-04-27 15:54:38 -0400

    added methods to store scraped data

--- KeyboardScraperBackend/Controllers/KeyboardsController.cs
+++ KeyboardScraperBackend/Controllers/KeyboardsController.cs
@@ -36,7 +36,7 @@ namespace KeyboardScraperBackend.Controllers
         [HttpGet("{EbayId}", Name = "GetKeyboardEbayId")]
         public IActionResult GetKeyboardEbayId(string EbayId)
         {
-            return Ok(_keyboardServices.getKeyboardEbayId(EbayId));
+            return Ok(_keyboardServices.GetKeyboardEbayId(EbayId));
         }
 
         [HttpPost]
@@ -44,6 +44,32 @@ namespace KeyboardScraperBackend.Controllers
         {
             _keyboardServices.AddKeyboard(kb);
             return CreatedAtRoute("GetKeyboardEbayId", new { EbayId = kb.EbayId }, kb);
+        }
+
+        /*[HttpDelete("{id}")]
+        public IActionResult DeleteKeyboard(string id)
+        {
+            _keyboardServices.DeleteKeyboard(id);
+            return NoContent();
+        }*/
+        
+        [HttpDelete("{EbayId}")]
+        public IActionResult DeleteKeyboardEbayId(string EbayId)
+        {
+            _keyboardServices.DeleteKeyboardEbayId(EbayId);
+            return NoContent();
+        }
+
+        /*[HttpPut]
+        public IActionResult UpdateBook(EbayKeyboard kb)
+        {
+            return Ok(_keyboardServices.UpdateKeyboard(kb));
+        }*/
+
+        [HttpPut]
+        public IActionResult StoreAndUpdate(List<EbayKeyboard> KbList)
+        {
+            return Ok(_keyboardServices.StoreAndUpdate(KbList));
         }
 
     }
